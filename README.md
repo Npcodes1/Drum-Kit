1d5063a345dbbfbbbda476af6d24b465835625be
Nicole Payne <nicole1rock@gmail.com>
2023-08-01 17:29:33 -0500

    Edit ReadMe and fixed css links for drum images

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Drum Kit
 - This drum Kit was created while completing a section on Dr. Yu's The Complete 2023 Web Development Bootcamp. It incorporated HTML, CSS, and JavaScript.
 - Using the starting files, I was able to learn and add Event Listeners to buttons, learn about higher order functions, pass functions as arguments, JavaScript Objects, and apply sounds and animations on the website.
-- The goal was that each letter would have a drum image and drum sound. Upon either clicking the image or pressing the appropriate letter on the keyboard, the drum's sound would play.
 
-# Instructions
+# Purpose
+- The goal for this project was to make it so that each letter would have a drum image and drum sound. Upon either clicking the image or pressing the appropriate letter on the keyboard, the drum's sound would play.
+
+# Major Functions
+- This project uses the Document Object Model (DOM) to change the button of the drums to make sounds when pressed and to stop when not pressed. There are other functions used such as Event Listeners and button animations.
+
+# Deploy Instructions
 - Play some drums by either tapping the drum pictures or pressing the corresponding keyboard letters. The image will light up when it's been pressed and play the sound for that drum.
 
 # Installation
--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        
-      let buttonInnerHTML = this.innerHTML;
 
-      makeSound(buttonInnerHTML);
+        let buttonInnerHTML = this.innerHTML;
 
-      buttonAnimation(buttonInnerHTML);
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
 
     });
 
@@ -17,11 +17,11 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
 // For detecting Keyboard press
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
 
-   makeSound(event.key);
+    makeSound(event.key);
 
-   buttonAnimation(event.key);
+    buttonAnimation(event.key);
 
 });
 
@@ -29,37 +29,37 @@ function makeSound(key) {
     switch (key) {
         case "w":
             let kick = new Audio("sounds/kick-bass.mp3");
-            kick.play();                
+            kick.play();
             break;
-        
+
         case "a":
             let snare = new Audio("sounds/snare.mp3");
             snare.play();
             break;
-    
+
         case "s":
             let tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();                
+            tom1.play();
             break;
 
         case "d":
             let tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();                
+            tom2.play();
             break;
 
         case "j":
             let tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();                
+            tom3.play();
             break;
 
         case "k":
             let tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();                
+            tom4.play();
             break;
 
         case "l":
             let crash = new Audio("sounds/crash.mp3");
-            crash.play();                
+            crash.play();
             break;
 
         default: console.log(buttonInnerHTML);
@@ -67,14 +67,14 @@ function makeSound(key) {
 }
 
 function buttonAnimation(currentKey) {
-    
-   let activeButton = document.querySelector("." + currentKey);
 
-   activeButton.classList.add("pressed");
+    let activeButton = document.querySelector("." + currentKey);
 
-   setTimeout(function() {
-    activeButton.classList.remove("pressed");
-   }, 100);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
 
 
